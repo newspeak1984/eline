3708b2d9aa6ecfd361175f4ed0275db5f42a552c
Kaitlyn Lee <kaitlyn_l@rogers.com>
2020-05-03 15:21:44 -0400

    removed sqs stuff

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -1,33 +1,9 @@
 const router = require('express').Router();
 let Customer = require('../models/customer_model');
 let Store = require('../models/store_model');
-const { sendMessage } = require('../sqsQueues');
 
 router.route('/').get((req, res, next) => {
   
-});
-
-router.route('/:storeName/add_to_queue').get((req, res) => {
-  const storeName = req.params.storeName;
-  // TODO: get actual customerId (maybe form state?)
-  const customerId = "email@email.com";
-
-  Store.findOne({ name: storeName }, function(err, store) {
-    console.log('checking mongo');
-    if (err) {
-      console.log(err);
-    } else {
-      if (!store) {
-        console.log(`${storeName} does not exist`)
-      } else {
-        console.log(store.queueUrl);
-        sendMessage(store.queueUrl, storeName, customerId);
-        // TODO only send on sendMessage success 
-        res.send('message sent!');
-      }
-    }
-  })
-
 });
 
 module.exports = router;
