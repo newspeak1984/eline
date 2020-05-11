b83dbe570de3bcd926d908257cccb284b406d1d3
newspeak1984 <ivanchow2468@gmail.com>
2020-05-11 14:30:06 -0400

    final changes to customer UI

--- eline_client/src/components/ForgotPassword.js
+++ eline_client/src/components/ForgotPassword.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios';
+const logo = require('../graphics/eline.png')
+const baseDesign = require('../graphics/myhumps.png')
 
 export default function ForgotPassword() {
     const [recoverEmail, setRecoverEmail] = useState();
@@ -20,22 +22,40 @@ export default function ForgotPassword() {
             });
     }
 
+    const styles = {
+        "elineLogo": {
+            marginTop: '20px',
+            marginBottom: '33px',
+            width: '60%'
+        },
+        "resetText":{
+            textAlign: 'center',
+            fontFamily: 'Helvetica',
+            fontSize: '31px',
+            lineHeight: '42px',
+            marginTop: '30px',
+        },
+    }
+
     return (
-        <div>
-            <h1>Reset Password Form</h1>
+        <div style={{"textAlign": 'center'}}>
+            <img src={logo} class="elineLogo" style={styles.elineLogo}></img>
+            <p style={styles.resetText}>Reset Password Form</p>
             <form onSubmit={onSubmit}>
                 <div className="form-group">
-                    <label>Email: </label>
                     <input type="email"
+                        placeholder="Email"
                         required
-                        className="form-control"
+                        style={{"marginTop": '20px', "marginBottom": '20px'}}
+                        className="InputForm"
                         onChange={onChangeRecoverEmail}
                     />
                 </div>
                 <div className="form-group">
-                    <input type="submit" value="Send" className="btn btn-primary" />
+                    <input type="submit" value="Send" className="GreenButton" />
                 </div>
             </form>
+            <img src={baseDesign} class="fixBottom"></img>
         </div>
     )
 }
--- eline_client/src/components/ResetPassword.js
+++ eline_client/src/components/ResetPassword.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import queryString from 'query-string';
+const logo = require('../graphics/eline.png')
+const baseDesign = require('../graphics/myhumps.png')
 
 class ResetPassword extends React.Component {
     constructor(props) {
@@ -52,36 +54,54 @@ class ResetPassword extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Reset Password</h1>
+            <div style={{"textAlign": 'center'}}> 
+                <img src={logo} class="elineLogo" style={styles.elineLogo}></img>
+                <p style={styles.resetText}>Reset Password</p>
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
-                        <label>New Password: </label>
                         <input type="password"
+                            placeholder="New Password"
                             id="password"
                             required
-                            className="form-control"
+                            className="InputForm"
                             value={this.newPassword}
                             onChange={this.onChangeNewPassword}
                         />
                     </div>
                     <div className="form-group">
-                        <label>Confirm Password: </label>
                         <input type="password"
+                            placeholder="Confirm Password"
                             id="password_confirm"
                             required
                             onInput={this.check}
-                            className="form-control"
+                            className="InputForm"
                             value={this.confirmPassword}
                             onChange={this.onChangeConfirmPassword}
                         />
                     </div>
                     <div className="form-group">
-                        <input type="submit" value="Send" className="btn btn-primary" />
+                        <input type="submit" value="Send" className="GreenButton" />
                     </div>
                 </form>
+                <img src={baseDesign} class="fixBottom"></img>
             </div>
         )
     }
 }
+
+const styles = {
+    "elineLogo": {
+        marginTop: '20px',
+        marginBottom: '33px',
+        width: '60%'
+    },
+    "resetText":{
+        textAlign: 'center',
+        fontFamily: 'Helvetica',
+        fontSize: '31px',
+        lineHeight: '42px',
+        marginTop: '30px',
+    },
+}
+
 export default ResetPassword;
